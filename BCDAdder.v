388fc83c0085076e0b7a356eccca1a17513b3734
AmirSameh1 <amirsameh770@gmail.com>
2025-02-04 16:18:42 +0200

    Update BCDAdder.v

--- BCDAdder.v
+++ BCDAdder.v
@@ -5,9 +5,8 @@ output[4:0]Sum);
 
 wire [4:0]w1;
 
-FourBitAdder FA1(.A(A),.B(B), .Cin(Cin), .Sum(w1));
-FourBitAdder FA2(.A(w1[3:0]),.B({1'b0,Sum[4] ,Sum[4] ,1'b0}), .Cin(0), .Sum(Sum[3:0]));
+Adder#(.Width(4)) FA1(.A(A),.B(B), .Cin(Cin), .Sum(w1));
+Adder#(.Width(4)) FA2(.A(w1[3:0]),.B({1'b0,Sum[4] ,Sum[4] ,1'b0}), .Cin(0), .Sum(Sum[3:0]));
 assign Sum[4]= w1[4] |(w1[3]&w1[2]) | (w1[3]&w1[1]);
 
 endmodule
-
